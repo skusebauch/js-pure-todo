202bab8285997f3d11bf2005f1042ef64dc3dd26
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-07-03 14:25:28 +0200

    added js function event listener and selectors

---
+++ app.js
@@ -0,0 +1,49 @@
+// selectors
+const todoInput = document.querySelector(".todo__input");
+const todoButton = document.querySelector(".todo__button");
+const todoList = document.querySelector(".todo__list");
+
+// Event Listener
+
+todoButton.addEventListener("click", addTodo);
+
+// Functions
+function addTodo(event) {
+  // prevent default behaviour due to submitting
+  event.preventDefault();
+
+  // want to build this:
+  //  <div class="todo__item">
+  //    <li class="todo__descr"></li>
+  //    <button>delete</button>
+  //    <button>checked</button>
+  //   </div>
+
+  // todo div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo__item");
+
+  // create li
+  const newTodo = document.createElement("li");
+  newTodo.innerText = "hey this is just test";
+  newTodo.classList.add("todo__descr");
+  // I take the div and put the li item inside
+  todoDiv.appendChild(newTodo);
+
+  // check mark button
+  const completedButton = document.createElement("button");
+  // to add itag to the button not text - need to use innerHTML instead of innerTEXT
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("btn-completed");
+  todoDiv.appendChild(completedButton);
+
+  // check trash button
+  const trashButton = document.createElement("button");
+  // to add itag to the button not text - need to use innerHTML instead of innerTEXT
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("btn-trash");
+  todoDiv.appendChild(trashButton);
+
+  // append all to ul list
+  todoList.appendChild(todoDiv);
+}
